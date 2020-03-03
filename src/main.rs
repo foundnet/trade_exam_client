1449ee5a28daff66681f4a536e00994b6a0ee549
Kai Liu <foundnet@hotmail.com>
2020-03-03 22:48:19 +0800

    Add struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 
+enum TradeDirection {
+    buy,
+    sell,
+}
 
 struct NewOrderSingle {
-    account_id: [char,10];
-    client_ord_id: u64;
-    
+    account_id: [char;10],
+    client_ord_id: u64,
+    security_id: [char;8],
+    direction: char,
+    price: i64,
+    count: i64,
 }
 
 fn main() {
