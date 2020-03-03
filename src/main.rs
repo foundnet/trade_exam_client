5cdd46e816736ed1cbe4dc1e1c109b4b8dfab856
foundnet <foundnet@foundnetdeMacBook-Pro.local>
2020-03-03 17:19:04 +0800

    3/2 off work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+
+struct NewOrderSingle {
+    account_id: [char,10];
+    client_ord_id: u64;
+    
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let s = String::from("hello world");
+
+let hello = &s[0..5];
+let world = &s[6..11];
+
+
 }
